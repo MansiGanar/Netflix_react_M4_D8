8bc9d2117dfefb185ea5b2321d6993cacae66021
Mansi Ganar <86571822+MansiGanar@users.noreply.github.com>
2021-11-17 20:41:37 +0100

    Commit

---
+++ netflix-react/src/App.js
@@ -0,0 +1,22 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Galleries from "./components/Galleries";
+import Navbarr from "./components/Navbarr";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import MyFooter from "./components/MyFooter";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbarr />
+      <Container fluid>
+        <Galleries />
+      </Container>
+      <MyFooter />
+    </div>
+  );
+}
+
+export default App;
